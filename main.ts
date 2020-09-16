d68244d792e696a43597247aa1fa8379c8e1b178
Nina-Mathew <71400436+Nina-Mathew@users.noreply.github.com>
2020-09-16 14:23:09 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,19 @@
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 4; index++) {
-        basic.showIcon(IconNames.Square)
+        radio.setGroup(21)
+        basic.showIcon(IconNames.Heart)
         basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
+            # # # # #
+            . # . # #
+            . . # . .
+            . # . # .
+            # . . . #
             `)
+        basic.showString("Say my name")
+        music.playMelody("C E G E F E D C ", 270)
     }
 })
+led.toggle(led.brightness(), 3)
 basic.forever(function () {
-    basic.showLeds(`
-        # # # # #
-        # . . . #
-        # . . . #
-        # . . . #
-        # # # # #
-        `)
+	
 })
